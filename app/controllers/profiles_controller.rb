62900e678401e796e86aca6dd6c4b1b9549fa340
TRAN Kim Ngoc <ngoc.frcnam@gmail.com>
2015-06-22 19:29:18 +0000

    Corrected syntax errors for profiles controller and its model

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ActionController::Base
   def new
     # form where user can fill out their OWN profile
-    @user = User.find( params [:user_id] )
+    @user = User.find( params[:user_id] )
     @profile = @user.build_profile
   end
 end
