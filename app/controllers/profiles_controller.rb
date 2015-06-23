56c4fab8dd8850ce3f3b0cc311266f3df4bd7fff
TRAN Kim Ngoc <ngoc.frcnam@gmail.com>
2015-06-23 09:31:05 +0000

    Changed new action in profilescontroller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def new
     # form where user can fill out their OWN profile
     @user = User.find(params[:user_id] )
-    @profile = @user.build_profile
+    @profile = Profile.new
   end
   def create
     @user = User.find(params[:user_id])
