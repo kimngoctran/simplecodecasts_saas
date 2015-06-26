33f365a68c211081ef45779450dfd27272686950
TRAN Kim Ngoc <ngoc.frcnam@gmail.com>
2015-06-26 08:34:55 +0000

    Added index action into user controller and its view to show a list of user in community page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def index
+    #Use.joints(:profile) to take only users who have profile. for more info, search Active Record Query Interface
+    @users = User.all.joins(:profile)
+  end
 end
